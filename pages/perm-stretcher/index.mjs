24e78e6c571c32952939225430e25d69dcc3a8e3
ilyaigpetrov <ilyaigpetrov@gmail.com>
2019-06-13 11:26:18 +0500

    Make a page with checkable optional permissions

---
+++ pages/perm-stretcher/index.mjs
@@ -0,0 +1,39 @@
+import { optional } from '../../permissions.mjs';
+
+(async () => {
+
+  toBeRequested.innerHTML = (await Promise.all(optional.map((perm) =>
+    new Promise((resolve) =>
+      chrome.permissions.contains({
+        permissions: [perm],
+      }, (ifGranted) =>
+        resolve(`
+<li>
+  <input type="checkbox" ${ifGranted ? 'checked' : ''} data-perm="${perm}" id="perm-${perm}"><label for="perm-${perm}">${perm}</label>
+</li>`
+      )),
+    ),
+
+))).join('\n');
+
+  toBeRequested.onchange = (event) => {
+
+    const input = event.target;
+    if(input.tagName !== 'INPUT') {
+      return;
+    }
+    const perm = input.dataset.perm;
+    chrome.permissions[input.checked ? 'request' : 'remove']({
+      permissions: [perm],
+    }, function(granted) {
+      if (!granted) {
+        input.checked = !input.checked;
+        const err = chrome.runtime.lastError;
+        const msg = err && err.msg;
+        alert(`Change denied. Reason: ${msg || 'unknown'}.`);
+      }
+    });
+  };
+
+})();
+
